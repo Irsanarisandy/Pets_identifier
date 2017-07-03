eaf3c1989d048cbc9f6599586634e3fd4ae5a3c7
Irsan Arisandy <irsanarisandy@hotmail.com>
2017-07-03 22:40:29 +1200

    Integrating Camera and Custom Vision

--- Pets_identifier/Pets_identifier.Android/MainActivity.cs
+++ Pets_identifier/Pets_identifier.Android/MainActivity.cs
@@ -1,11 +1,10 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.Permissions;
 
 namespace Pets_identifier.Droid
 {
@@ -22,6 +21,10 @@ namespace Pets_identifier.Droid
 			global::Xamarin.Forms.Forms.Init(this, bundle);
 			LoadApplication(new App());
 		}
+
+		public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+		{
+			PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+		}
 	}
 }
-
--- Pets_identifier/Pets_identifier/CustomVision.xaml.cs
+++ Pets_identifier/Pets_identifier/CustomVision.xaml.cs
@@ -1,20 +1,94 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Pets_identifier.Model;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.IO;
 using System.Linq;
-using System.Text;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace Pets_identifier
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class CustomVision : ContentPage
 	{
 		public CustomVision()
 		{
 			InitializeComponent();
+		}
+
+		private async void LoadCamera(object sender, EventArgs e)
+		{
+			await CrossMedia.Current.Initialize();
+
+			if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+			{
+				await DisplayAlert("No Camera", ":( No camera available.", "OK");
+				return;
+			}
+
+			MediaFile file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+			{
+				PhotoSize = PhotoSize.Medium,
+				Directory = "Sample",
+				Name = $"{DateTime.UtcNow}.png"
+			});
+
+			if (file == null)
+				return;
+
+			image.Source = ImageSource.FromStream(() =>
+			{
+				return file.GetStream();
+			});
+
+			 await MakePredictionRequest(file);
+        }
+
+        static byte[] GetImageAsByteArray(MediaFile file)
+        {
+            var stream = file.GetStream();
+            BinaryReader binaryReader = new BinaryReader(stream);
+            return binaryReader.ReadBytes((int)stream.Length);
+        }
+
+		async Task MakePredictionRequest(MediaFile file)
+		{
+			var client = new HttpClient();
+
+			client.DefaultRequestHeaders.Add("Prediction-Key", "69ac7f16e50a4c8fa1f8fa2f7e882d1e");
+
+			string url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v1.0/Prediction/821dc763-ec21-420a-986e-30ccc435bda6/image?iterationId=6092ed69-5fb9-458b-b9e6-5c059404a273";
+
+			HttpResponseMessage response;
+
+			byte[] byteData = GetImageAsByteArray(file);
+
+			using (var content = new ByteArrayContent(byteData))
+			{
+
+				content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+				response = await client.PostAsync(url, content);
+
+
+				if (response.IsSuccessStatusCode)
+				{
+					var responseString = await response.Content.ReadAsStringAsync();
+
+					EvaluationModel responseModel = JsonConvert.DeserializeObject<EvaluationModel>(responseString);
+
+					double max = responseModel.Predictions.Max(m => m.Probability);
+
+					TagLabel.Text = (max >= 0.5) ? "Pet" : "Not pet";
+
+				}
+
+				//Get rid of file once we have finished using it
+				file.Dispose();
+			}
 		}
 	}
 }
